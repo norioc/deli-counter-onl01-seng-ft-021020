3ba5f4ea987c58870d73223235b8c7c35df9a32f
norio c <norionorioc@gmail.com>
2020-02-12 21:59:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,11 @@ def line (array)
    if array.count == 0
       puts "The line is currently empty."
    else
-      return = "The line is currently:"
+      response = "The line is currently:"
       array.each_with_index do |name, index|
-         return += " #{index+1}. #{name}" 
+      response += " #{index+1}. #{name}" 
    end
-   puts return
+   puts response
    end
 end
 
