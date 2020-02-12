4ba70b93b4714108e66783a832ce4f6c39ad9cb5
norio c <norionorioc@gmail.com>
2020-02-12 22:03:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,28 @@
 
 katz_deli=[]
 
-def line (array)
-   if array.count == 0
+def line (array)  
+  
+  if array.count == 0
       puts "The line is currently empty."
-   else
+  else
       response = "The line is currently:"
       array.each_with_index do |name, index|
-      response += " #{index+1}. #{name}" 
-   end
-   puts response
-   end
+      response += " #{index+1}. #{name}"
+    end
+    
+  puts response
+  
+  end
 end
 
 def take_a_number(array, name)
+  
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
   
   end
+
+def
 
   
