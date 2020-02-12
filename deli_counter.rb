ba50a062187603eafe4dace495c7e99aa7226bcd
norio c <norionorioc@gmail.com>
2020-02-12 21:57:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,16 @@
 
 katz_deli=[]
 
-def line(array)
-  
-  line_array = [] 
-  if array.count == 0
-  puts "The line is currently empty."
-  
-  else 
-  array.each_with_index(0) do |name, index|
-  line_array.push("#{index}. #{name}")
-  end
-  
-  puts "This line is currently: #{line_array.join(" ")}"
-
-  end  
-  
+def line (array)
+   if array.count == 0
+      puts "The line is currently empty."
+   else
+      response = "The line is currently:"
+      array.each_with_index do |name, index|
+         response += " #{index+1}. #{name}" 
+   end
+   puts response
+   end
 end
 
   
